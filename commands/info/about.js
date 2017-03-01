d271d627b82f352c6d1ffc3f0b0dada292227ab5
gloss-water <gloss.water@gmail.com>
2017-03-01 06:07:33 +0000

    add stats command, switch to rich embeds

--- commands/info/about.js
+++ commands/info/about.js
@@ -1,3 +1,4 @@
+const { RichEmbed } = require('discord.js');
 const { Command } = require('discord.js-commando');
 const { stripIndents } = require('common-tags');
 
@@ -9,29 +10,33 @@ class About extends Command {
             memberName: 'about',
             description: 'Displays information about the bot.',
             throttling: {
-                usages: 2,
-                duration: 3
+                usages: 1,
+                duration: 10
             }
         });
     }
 
     async run(msg) {
-        return msg.embed({
-            color: 8700043,
-            description: stripIndents`
-                __**Cassandra v2.0 (QASI)**__
-                Cassandra v2.0, code name 'QASI', is a complete rewrite of cassandra.js.
-                Cassie was a simple discord.js bot used for administration / image search.
-                Now, it harnesses the power of discord.js-commando to make it easier to
-                use, maintain, and update.
+        return msg.embed(new RichEmbed()
+                .setTitle(`__**Cassandra v2.0 (QASI)**__`)
+                .setFooter(`requested by ${msg.author.username}`, msg.author.avatarURL)
+                .setDescription(stripIndents`
+                    Cassandra v2.0, code name 'QASI', is a complete rewrite of cassandra.js.
+                    
+                    Cassie was a simple discord.js bot used for administration / image search.
+                    
+                    Now, it harnesses the power of discord.js-commando to make it easier to
+                    use, maintain, and update.
 
-                [qasi on github](https://github.com/gloss-water/qasi)
+                    [qasi on github](https://github.com/gloss-water/qasi)
 
-                [Discord.js](https://github.com/hydrabolt/discord.js)
-                [Commando](https://github.com/Gawdl3y/discord.js-commando)
-                [Commando bot](https://github.com/WeebDev/Commando)
-            `
-        });
+                    [Discord.js](https://github.com/hydrabolt/discord.js)
+                    [Commando](https://github.com/Gawdl3y/discord.js-commando)
+                    [Commando bot](https://github.com/WeebDev/Commando)
+                `)
+                .setImage(this.client.user.displayAvatarURL)
+                .setColor(8700043)
+        );
     }
 }
 
---
+++ commands/info/stats.js
@@ -0,0 +1,47 @@
+const { RichEmbed } = require('discord.js');
+const { Command } = require('discord.js-commando');
+const { stripIndents } = require('common-tags');
+
+const plural = (a, p) => {
+    return a !== 1 ? p + 's ' : p + ' ';
+}
+
+const uptime = (s) => {
+    const day = (s/86400000|0);
+    const hour = (s%86400000)/3.6e6|0;
+    const min = (s%3.6e6)/6e4|0;
+    const sec = (s%6e4)/1000|0;
+    return day + plural(day, ' day') + hour + plural(hour, ' hour') + min + plural(min, ' minute') + sec + plural(sec, ' second');
+};
+
+class Stats extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'stats',
+            group: 'info',
+            memberName: 'stats',
+            description: 'Displays Cassandra\'s current statistics.',
+            throttling: {
+                usages: 1,
+                duration: 10
+            }
+        });
+    }
+
+    async run(msg) {
+        return msg.embed(new RichEmbed()
+                .setTitle(`Cassandra 2.0 statistics! ^_^`)
+                .setFooter(`requested by ${msg.author.username}`, msg.author.avatarURL)
+                .setDescription(`
+                    Uptime: ${uptime(this.client.uptime % 8.64e7)}
+                    Ping: ${Math.round(msg.client.ping)}ms
+                    Channels: ${this.client.channels.size}
+                    Members: ${this.client.users.size}
+                    `)
+                .setImage(this.client.user.displayAvatarURL)
+                .setColor(8700043)
+        );
+    }
+}
+
+module.exports = Stats;

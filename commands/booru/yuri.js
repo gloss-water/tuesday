3dc1f73a900b5e106042d9cf6bb2a659f610b9ce
morosemagician <odinn08@gmail.com>
2017-03-06 03:47:42 +0000

    Removed the yuri command and added a yuri command
    Instead of saying "hey" it now actually does a thing, wowie!

---
+++ commands/booru/yuri.js
@@ -0,0 +1,29 @@
+const { safeSearch, safeResults } = require('./search');
+const { Command } = require('discord.js-commando');
+const config = require('../../data/config');
+
+class Yuri extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'yuri',
+            aliases: ['yoi'],
+            group: 'booru',
+            memberName: 'yuri',
+            description: 'Searches for a picture tagged yuri_on_ice on safebooru.',
+            throttling: {
+                usages: 1,
+                duration: 10
+            }
+        });
+    }
+
+    async run(message, args) {
+        if (config.search.indexOf(message.channel.id) === -1) return message.reply(`this command only works in search enabled channels.`);
+        safeSearch(('yuri_on_ice ' + args).replace('+','%2B').replace(' ', '+'))
+            .then(result => safeResults(result, message), err => {
+                console.log(err);
+            });
+    }
+}
+
+module.exports = Yuri;
